962f705d1045f59ef1951b0231fc219984cb355f
Vorobev Vsevolod <100051617+vorobevvsevolod@users.noreply.github.com>
2024-03-28 08:44:48 +0300

    1.2.3

--- server.js
+++ server.js
@@ -16,12 +16,11 @@ app.use(cors())
 app.use(express.json())
 
 // //Фотографии
-
-
+// // redux.use(express.static(path.resolve(__dirname, 'static/product')))
+// // redux.use(express.static(path.resolve(__dirname, 'static/dopProduct')))
 app.use(express.static(path.resolve(__dirname, 'static/categoryIMG')))
-app.use(express.static(path.resolve(__dirname, 'static/WorksIMG')))
 app.use(express.static(path.resolve(__dirname, 'static/GalleryWorks')))
-
+app.use(express.static(path.resolve(__dirname, 'static/WorksIMG')))
 app.use('/admin', adminRouter)
 
 
@@ -34,7 +33,7 @@ app.use(errorHandler)
 const start = async () => {
 	try {
 		await sequelize.authenticate()
-		await sequelize.sync({force:true})
+		await sequelize.sync()
 		app.listen(PORT, () => console.log(`Сервер запущен на ${PORT} порту`))
 	}catch (e) { console.log(e.message); }
 }
